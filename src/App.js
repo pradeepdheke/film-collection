ae8209973d320bc2a7c81b073b614e96cedf0170
pradeep dheke <pradeepbarca029@gmail.com>
2022-06-16 15:54:38 +1000

    adding alert features when movies not found

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import {Alert, Container } from 'react-bootstrap';
 import './App.css';
 import { CustomCard } from './components/CustomCard';
 import { MovieList } from './components/MovieList';
 import { SearchForm } from './components/SearchForm';
 import {fetchMovieInfo} from './helper/axiosHelper'
+import a from './a.mp3'
 
 function App() {
+  const [audio] = useState(new Audio(a))
 const [movie, setMovie] = useState({})
+const [showError, setShowError] = useState("")
 
   const handleOnSubmit = async (str) => {
 console.log(str)
 
 const result = await fetchMovieInfo(str)
-console.log(result)  
+
 setMovie(result)
+
+  result.Response === "False" ? setShowError(result.Error) : setShowError("")
+
 }
-console.log(movie)
+
   return (
     <div className="wrapper">
       <Container>
         <SearchForm handleOnSubmit={handleOnSubmit} />
         <div className='mt-4 d-flex justify-content-center'>
           {movie.imdbID && <CustomCard movie = {movie}/>}
+          
+           {showError && audio.play(a) &&<Alert variant = "danger">{showError}</Alert>}
+          
         </div>
 
         <hr />
+              <MovieList/>
 
-        <MovieList/>
       </Container>
     </div>
   );
